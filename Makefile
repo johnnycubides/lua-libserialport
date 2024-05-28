e1f467d9c1f6ad1c04aba62269f064af0f735290
Johnny Cubides <jgcubidesc@gmail.com>
2024-05-28 10:48:04 -0500

    update scripts

--- Makefile
+++ Makefile
@@ -4,4 +4,9 @@ lua-linux:
 	bash ./scripts/lua.bash download
 	bash ./scripts/lua.bash build-linux
 	bash ./scripts/lua.bash install-linux
+
+libserialport-linux:
+	@echo "Descargar y crear librería libserialport"
+	bash ./scripts/libserialport.bash download
+	bash ./scripts/libserialport.bash build-linux
 	
---
+++ scripts/libserialport.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+APP=libserialport
+SCRIPT_PATH=$(pwd)
+BUILDER_PATH=$SCRIPT_PATH/src
+DIR_APP=$BUILDER_PATH/$APP
+
+dependencies() {
+	sudo apt update
+	sudo apt install \
+		libtool \
+		-y
+}
+
+download() {
+	mkdir -p $BUILDER_PATH
+	cd $BUILDER_PATH
+	rm -rf $DIR_APP
+	git clone git@github.com:sigrokproject/libserialport.git $APP
+}
+
+build-linux() {
+	cd $DIR_APP
+	dependencies
+	./autogen.sh
+	./configure --enable-static --disable-shared
+	make
+}
+
+if [[ -v 1 ]]; then
+	$1
+fi
